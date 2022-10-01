ca86c2be1a9cf8668cf91dcf8460ed4662542163
Bianca Popescu <inktense12@gmail.com>
2022-10-01 22:21:18 +0100

    Got table rows

--- README.md
+++ README.md
@@ -3,3 +3,14 @@ Script used to search Amazon Kindle book deals for books marked as "Want to read
 
 
 AWS_PROFILE=PROFILE sls deploy
+
+
+work locally:
+
+sls offline
+
+change terminal
+
+aws lambda invoke /local \
+  --endpoint-url http://localhost:3002 \
+  --function-name goodreads-amazon-cron-function
--- src/services/puppeteer.ts
+++ src/services/puppeteer.ts
@@ -14,7 +14,53 @@ export const getToReadShelf = async () => {
         const browser = await puppeteer.launch({headless: false});
         const page = await browser.newPage();
         await page.setViewport({ width: 1366, height: 768});
-        await page.goto(`https://www.goodreads.com/review/list/${process.env.GOODREADS_USER}?shelf=to-read`);
+        await page.goto(`https://www.goodreads.com/review/list/${process.env.GOODREADS_USER}?shelf=to-read`,
+        {waitUntil: 'networkidle2'}
+        );
+
+       // await sleep_for(page, 1000, 2000)
+        const books = await page.$x(`//div[@class="myBooksPage"]/div[2]/div[2]/table/tbody`)
+        let lines: string[] = []
+
+        const rawData = await page.evaluate(() => {
+            let data = []
+            let table = document.getElementById('booksBody')
+            for (var i = 1; i < table.rows.length; i++) {
+                let objCells = table.rows.item(i).cells;
+        
+                let values = [];
+                for (var j = 0; j < objCells.length; j++) {
+                  let text = objCells.item(j).innerHTML;
+                  values.push(text);
+                }
+                let d = { i, values };
+                data.push(d);
+              }
+        return data
+        })
+
+        console.log(rawData)
+
+    //     const scrapedData = await page.evaluate(() =>
+    //     Array.from(document.querySelectorAll('h4 a'))
+    //       .map(link => ({
+    //         title: link.innerHTML,
+    //         link: link.getAttribute('href')
+    //       }))
+    //   )
+
+        // if (books.length > 0) {
+        //     for (let i = 0; i <= books.length; i ++) {
+        //         let title = await page.evaluate(el => el.innerText, books[i]);
+        //         title = title.replace(/(\r\n|\n\r)/gm, ' ')
+        //         console.log("Here title => ", title)
+        //         lines.push(title)
+        //     }
+        // }
+
+        console.table(lines)
+
+
 
     } catch (error) {
         console.log("Puppeteer error: ", error)
