d3ea6718d9d7c97d59b4786030ecc33816eb5f96
Bianca Popescu <inktense12@gmail.com>
2022-10-05 20:23:27 +0100

    Got all the book titles and authors

--- .build/src/services/puppeteer.js
+++ .build/src/services/puppeteer.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.getToReadShelf = void 0;
 const puppeteer_1 = require("puppeteer");
 const goodreads_1 = require("../constants/goodreads");
+const puppeteer_2 = require("../helpers/puppeteer");
 //Promise<Book[]>
 const getToReadShelf = async () => {
     try {
@@ -14,41 +15,29 @@ const getToReadShelf = async () => {
             deviceScaleFactor: 1,
         });
         await page.goto(goodreads_1.URL, { waitUntil: "networkidle2" });
+        await page.waitForTimeout(5000);
+        // Scroll to the bottom of the page to make sure we get all the books
+        await (0, puppeteer_2.autoScroll)(page);
+        // Waiting to make sure we manage to scroll to the bottom,
+        // otherwise we will miss the last books
+        await page.waitForTimeout(10000);
         // await sleep_for(page, 1000, 2000)
-        const books = await page.$x(`//div[@class="myBooksPage"]/div[2]/div[2]/table/tbody`);
-        let lines = [];
-        const rawData = await page.evaluate(() => {
-            let data = [];
-            let table = document.getElementById("booksBody");
-            for (var i = 1; i < table.rows.length; i++) {
-                let objCells = table.rows.item(i).cells;
-                let values = [];
-                for (var j = 0; j < objCells.length; j++) {
-                    let text = objCells.item(j).innerHTML;
-                    values.push(text);
-                }
-                let d = { i, values };
-                data.push(d);
-            }
-            return data;
+        // const books = await page.$x(
+        //   `//div[@class="myBooksPage"]/div[2]/div[2]/table/tbody`
+        // );
+        // let lines: string[] = [];
+        const titlesArray = await page.evaluate(() => {
+            // const tds = Array.from(document.querySelectorAll(' tr td'))
+            const titles = Array.from(document.getElementsByClassName("field title"));
+            // const authors = Array.from(document.getElementsByClassName('field author'))
+            return titles.map((title) => title.innerText);
         });
-        console.log(rawData);
-        //     const scrapedData = await page.evaluate(() =>
-        //     Array.from(document.querySelectorAll('h4 a'))
-        //       .map(link => ({
-        //         title: link.innerHTML,
-        //         link: link.getAttribute('href')
-        //       }))
-        //   )
-        // if (books.length > 0) {
-        //     for (let i = 0; i <= books.length; i ++) {
-        //         let title = await page.evaluate(el => el.innerText, books[i]);
-        //         title = title.replace(/(\r\n|\n\r)/gm, ' ')
-        //         console.log("Here title => ", title)
-        //         lines.push(title)
-        //     }
-        // }
-        console.table(lines);
+        const authorsArray = await page.evaluate(() => {
+            const authors = Array.from(document.getElementsByClassName("field author"));
+            return authors.map((author) => author.innerText);
+        });
+        console.table(titlesArray);
+        console.table(authorsArray);
     }
     catch (error) {
         console.log("Puppeteer error: ", error);
--- src/services/puppeteer.ts
+++ src/services/puppeteer.ts
@@ -1,12 +1,14 @@
 import puppeteer from "puppeteer";
 
 import { URL } from "../constants/goodreads";
+import { autoScroll } from "../helpers/puppeteer";
 
 type Book = {
   title: String;
   author: string;
 };
 //Promise<Book[]>
+
 export const getToReadShelf = async () => {
   try {
     const browser = await puppeteer.launch({ headless: false });
@@ -18,50 +20,40 @@ export const getToReadShelf = async () => {
     });
 
     await page.goto(URL, { waitUntil: "networkidle2" });
+    await page.waitForTimeout(5000);
+
+    // Scroll to the bottom of the page to make sure we get all the books
+    await autoScroll(page);
+
+    // Waiting to make sure we manage to scroll to the bottom,
+    // otherwise we will miss the last books
+    await page.waitForTimeout(10000);
 
     // await sleep_for(page, 1000, 2000)
-    const books = await page.$x(
-      `//div[@class="myBooksPage"]/div[2]/div[2]/table/tbody`
-    );
-    let lines: string[] = [];
+    // const books = await page.$x(
+    //   `//div[@class="myBooksPage"]/div[2]/div[2]/table/tbody`
+    // );
+    // let lines: string[] = [];
 
-    const rawData = await page.evaluate(() => {
-      let data = [];
-      let table = document.getElementById("booksBody");
-      for (var i = 1; i < table.rows.length; i++) {
-        let objCells = table.rows.item(i).cells;
-
-        let values = [];
-        for (var j = 0; j < objCells.length; j++) {
-          let text = objCells.item(j).innerHTML;
-          values.push(text);
-        }
-        let d = { i, values };
-        data.push(d);
-      }
-      return data;
+    const titlesArray = await page.evaluate(() => {
+      // const tds = Array.from(document.querySelectorAll(' tr td'))
+      const titles = Array.from(document.getElementsByClassName("field title"));
+      // const authors = Array.from(document.getElementsByClassName('field author'))
+      return titles.map((title) => title.innerText);
     });
 
-    console.log(rawData);
+    const authorsArray = await page.evaluate(() => {
+      const authors = Array.from(
+        document.getElementsByClassName("field author")
+      );
+      return authors.map((author) => author.innerText);
+    });
 
-    //     const scrapedData = await page.evaluate(() =>
-    //     Array.from(document.querySelectorAll('h4 a'))
-    //       .map(link => ({
-    //         title: link.innerHTML,
-    //         link: link.getAttribute('href')
-    //       }))
-    //   )
+    console.table(titlesArray);
 
-    // if (books.length > 0) {
-    //     for (let i = 0; i <= books.length; i ++) {
-    //         let title = await page.evaluate(el => el.innerText, books[i]);
-    //         title = title.replace(/(\r\n|\n\r)/gm, ' ')
-    //         console.log("Here title => ", title)
-    //         lines.push(title)
-    //     }
-    // }
+    console.table(authorsArray);
 
-    console.table(lines);
+    
   } catch (error) {
     console.log("Puppeteer error: ", error);
   }
