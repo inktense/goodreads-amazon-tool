9e183eeec028173447e6e3c1aeab76f088722b0f
Bianca Popescu <inktense12@gmail.com>
2022-11-29 09:46:52 +0000

    Found discounted books from goodreads list

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,9 @@ const { getToReadShelf } = require("./services/puppeteer");
 const { writeDataToFiles } = require("./helpers/helpers");
 const { amazonOptions } = require("./constants/axios");
 
+const { arrayMock } = require("./constants/mock");
+const { Book } = require("./constants/types");
+
 const server = async () => {
   try {
     const toReadData = await getToReadShelf();
@@ -14,49 +17,60 @@ const server = async () => {
 
     writeDataToFiles(toReadData);
 
-    const promises = [];
-    const amazonBooks = [] as any;
-    let array1 = [] as any
+    // const promises = [];
+    // const amazonBooksPerPage = [] as any;
+    // let amazonBooks = [] as any;
 
-    for(let i = 1; i < 3; i++) {
-   //   console.log('amazonOptions', amazonOptions);
-      amazonOptions.params.page = i;
-      promises.push(axios.request(amazonOptions));
-    }
-console.log(promises)
-    await Promise.all(promises).then((response): [any] => {
-      
-      for(let i = 0; i < response.length; i++) {
-       // console.log('array 1', array1);
-       // console.log("here ", response[i].data.searchProductDetails);
-        array1 = amazonBooks.concat(response[i].data.searchProductDetails);
+    // for (let i = 1; i < 3; i++) {
+    //   amazonOptions.params.page = i;
+    //   promises.push(axios.request(amazonOptions));
+    // }
+
+    // await Promise.all(promises).then((response): [any] => {
+    //   for (let i = 0; i < response.length; i++) {
+    //     amazonBooks = amazonBooksPerPage.concat(
+    //       response[i].data.searchProductDetails
+    //     );
+    //   }
+    //   return amazonBooks;
+    // });
+
+    //console.log(arrayMock)
+    toReadData.push({
+      title: "The Grave Tattoo",
+      author: "author",
+    },
+    {
+      title: "Women with Adult ADHD",
+      author: "author",
+    });
+    const discountedBooks = [] as any;
+
+    toReadData.forEach((book: typeof Book) => {
+      const bookFound = arrayMock.find((element: any) => {
+        //console.log(element.productDescription, book.title);
+        return element.productDescription.includes(book.title);
+      });
+      if (bookFound) {
+        console.log("Found book: ", bookFound);
+        discountedBooks.push(bookFound);
       }
-      //console.log('array 2', array1.length, array1);
-      return array1;
-      
-    })
-    // console.log('a', a);
-    console.log('array 3', array1);
-    } catch (error) {
-      console.log("Server error: ", error);
-      throw Error(error as any);
-    };
+    });
 
-   
+    console.log("Found ", discountedBooks.length, " discounted books", discountedBooks);
 
+    const booksToBuy = discountedBooks.filter((book: any) => {
+      console.log(book.variations[0].value, book.variations.length, typeof book.variations)
+       return book.variations[0].value == "Kindle Edition" &&
+        book.variations[0].price < 1.00
+    
+  });
 
-    // axios
-    //   .request(amazonOptions)
-    //   .then(function (response: any) {
-    //     console.log(response.data);
-    //   })
-    //   .catch(function (error: any) {
-    //     console.error(error);
-    //   });
-  // } catch (error) {
-  //   console.error(`Error`);
-  //   throw Error(error as any);
-  // }
+    console.log("Books to buy: ", booksToBuy);
+  } catch (error) {
+    console.log("Server error: ", error);
+    throw Error(error as any);
+  }
 };
 
 server();
